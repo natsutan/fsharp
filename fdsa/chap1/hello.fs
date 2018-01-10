979fcdc6b5d752d97d6666693a08fd957d963030
natsutan <yrw03704@nifty.com>
2018-01-10 13:31:49 +0900

    work

--- fdsa/chap1/hello.fs
+++ fdsa/chap1/hello.fs
@@ -4,3 +4,12 @@ printfn "Hello World"
 let square n = n * n
 
 square 4
+
+// tapple
+let t = ("cats", "dogs", System.Math.PI, 42, "C", "Java")
+
+// array
+let GuardiansOfGalaxy = [| "Peter Quill"; "Gamora"; "Drax"; "Groot"; "Rocket"; "Ronan"; "Yondu Udonta"; "Nebula"; "Korath"; "Corpsman Dey";"Nova Prime";"The Collector";"Meredith Quill" |]
+
+GuardiansOfGalaxy.[4]
+
