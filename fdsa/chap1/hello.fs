c0d49e41bdc3b46d34fa259b51b4a383c80a51b4
natsutan <yrw03704@nifty.com>
2018-01-10 17:36:14 +0900

    chap1終了

--- fdsa/chap1/hello.fs
+++ fdsa/chap1/hello.fs
@@ -13,3 +13,50 @@ let GuardiansOfGalaxy = [| "Peter Quill"; "Gamora"; "Drax"; "Groot"; "Rocket"; "
 
 GuardiansOfGalaxy.[4]
 
+//range
+let OneToHundred = [|1..100|]
+
+//slice
+OneToHundred.[0..2]
+
+//patrially
+let add x y = x + y
+
+add 10 4
+
+let add10 = add 10
+
+add10 15
+
+//compsiton
+let inc n = n + 1
+let div n = n / 2
+
+let InvokeTri n (f:int->int) = f(f(f(n)))
+let InvokeTrif n (f:float->float) = f(f(f(n)))
+
+InvokeTri 10 inc
+InvokeTri 80 div
+InvokeTrif 2.0 (fun n -> n **3.0)
+
+//array
+let nums = [|0..99|]
+
+let squares =
+    nums |> Array.map (fun n -> n * n)
+
+//fold
+let sum = Array.fold(fun acc n -> acc + n) 0 squares
+
+//fillter
+let castNames = [| "Hofstadter"; "Cooper"; "Wolowitz"; "Koothrappali"; "Fowler"; "Rostenkowski";  |]
+
+let longNames = Array.filter (fun (name: string) -> name.Length > 6) castNames
+
+
+//zip
+let firstNames = [| "Leonard"; "Sheldon"; "Howard"; "Penny"; "Raj"; "Bernadette"; "Amy" |]
+let lastNames = [| "Hofstadter"; "Cooper"; "Wolowitz"; ""; "Koothrappali"; "Rostenkowski"; "Fowler" |]
+
+let fullNames = Array.zip(firstNames) lastNames
+
