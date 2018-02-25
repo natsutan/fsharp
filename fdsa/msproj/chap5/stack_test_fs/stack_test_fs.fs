796d28880c3d352806adf64ba9bdfb056d5ba70a
natsutan <YRW03704@nifty.com>
2018-02-25 09:57:16 +0900

    いったん終了

---
+++ fdsa/msproj/chap5/stack_test_fs/stack_test_fs.fs
@@ -0,0 +1,16 @@
+﻿// F# の詳細については、http://fsharp.org を参照してください
+// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+
+open NUnit.Framework
+open stack
+
+[<Test>]
+let stack_basic_test() =
+    let s = stack.Stack<int>();
+    s.Push 5
+    Assert.AreEqual(5, s.Pop)
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // 整数の終了コードを返します
